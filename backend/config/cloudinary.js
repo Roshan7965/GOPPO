5ab237f3973bc33769acb3b21b8b42fa1e485037
roshan tanpure <tanpureroshan03@example.com>
2025-09-27 13:41:45 +0530

    cloudinary setup is done

---
+++ backend/config/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+const uploadOnCloudinary=async(filePath)=>{
+    cloudinary.config(()=>{
+        cloudinary.config({ 
+        cloud_name: process.env.CLOUD_NAME, 
+        api_key: process.env.CLOUD_API_KEY, 
+        api_secret: process.env.CLOUD_API_SCERET, // Click 'View API Keys' above to copy your API secret
+    });
+    })
+
+    try {
+
+        const uploadResult=await cloudinary.uploader.upload(filePath);
+        fs.unlinkSync(filePath);
+        return uploadResult.secure_url;
+        
+    } catch (error) {
+        fs.unlinkSync(filePath);
+        console.log(error);
+        
+    }
+    
+}
+export default uploadOnCloudinary;
